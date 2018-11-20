37f522ae75ab2e4f2194f385c0e717463a456f09
neshka <neshka_mind@yahoo.com>
2018-11-20 21:41:54 +0000

    add obj dateAndMileage

---
+++ avarageMileage.js
@@ -0,0 +1,44 @@
+var regularExpression =
+
+{
+  //RRRR-M-DD //change it to consider cases when MM and D
+  datePattern : /\d{4}([\/.-])\d{1}\1\d{2}/g
+
+}
+
+function getListOfData (obj) {
+    var listOfValues = []
+    for (var i = 0; i<obj.length;i++){
+        if (history[i]["data"]["passed"] === true) {
+            listOfValues.push(obj[i]["date"])
+        }       
+    } return listOfValues
+}
+
+function getDate(array) {  
+    var matches = []
+    for (var i = 0; i < array.length; i++){
+        var str = array[i]
+        var match = str.match(regularExpression['datePattern'])
+        matches.push(match)
+    }
+    return matches                                                             
+}
+
+function getListOfMileage (obj) {
+    var listOfValues = []
+    for (var i = 0; i<obj.length;i++){
+        if (history[i]["data"]["passed"] === true) {
+            listOfValues.push(obj[i]["data"]["mileage"])
+        }       
+    } return listOfValues;
+}
+
+function dateWithMileage (dates, mileages) {
+    var dateAndMileage = {}
+    dates.forEach((date, i) => dateAndMileage[date] = mileages[i])
+
+    return dateAndMileage
+}
+
+
